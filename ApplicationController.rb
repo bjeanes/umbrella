aea2b6ed4e35b2508e5a0f4131fa2ec83f7314e6
Bodaniel Jeanes <me@bjeanes.com>
2009-02-27 23:21:38 +1000

    Open .torrent files using AppDelegate

---
+++ ApplicationController.rb
@@ -0,0 +1,24 @@
+#
+#  ApplicationController.rb
+#  Umbrella
+#
+#  Created by Bodaniel Jeanes on 27/02/09.
+#  Copyright (c) 2009 Mocra. All rights reserved.
+#
+
+class ApplicationController
+  attr_accessor :label
+  
+  def application(sender, openFile: filename)
+	application(sender, openFiles: [filename])
+  end
+  
+  def application(sender, openFiles: filenames)
+	puts "openFiles: #{filenames}"
+	filenames.each do |filename|
+		# send to utorrent API
+		
+		label.stringValue = "Received file #{filename}"
+	end
+  end
+end
