34cba00a55d443dc356a224c7a1e31757ae3bc8c
Bodaniel Jeanes <me@bjeanes.com>
2009-02-27 23:31:18 +1000

    JSON framework correctly loading into app (to_json method being defined on classes properly)

--- rb_main.rb
+++ rb_main.rb
@@ -12,6 +12,9 @@ framework 'Cocoa'
 
 # Loading all the Ruby project files.
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+
+require File.join(dir_path, 'json', 'json')
+
 Dir.entries(dir_path).each do |path|
   if path != File.basename(__FILE__) and path[-3..-1] == '.rb'
     require(path)
